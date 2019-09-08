8b669cecca570b0a8e706c186729b4d00f1b2212
Valentin Guibert <valentinguibert@hotmail.fr>
2019-09-08 14:27:05 +0200

    delete about page

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
   <div class="app">
     <div class="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
       <router-link to="/skills">Compétences</router-link>
     </div>
     <router-view class="animated fadeIn"/>
