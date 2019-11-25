0b9a8babd3ee854dbdf63f6edf11a4cfc19be1d4
Valentin Guibert <valentinguibert@hotmail.fr>
2019-11-25 14:40:39 +0100

    404 to home redirection

--- src/router.js
+++ src/router.js
@@ -26,6 +26,10 @@ export default new Router({
       path: '/about',
       name: 'about',
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    },
+    {
+      path: '*',
+      redirect : '/'
     }
 
   ]
